b6543b9ac1d123f8cd4cbbf3647e1c983bcffd86
@ihar_slabykho <yakuzawot6@gmail.com>
2025-11-20 21:03:34 +0300

    feat: javascript intro

---
+++ lesson_12/script.js
@@ -0,0 +1,73 @@
+// console.log("Hello world");
+
+// --------------------------------------------------------------- Переменные
+// var test1 - не используе, устарело;
+
+// let test2;
+// test2 = 'Hello world from variable test2';
+// test2 = 'override value';
+
+// console.log(test2);
+
+// const test3 = 'hellow world';
+// test3 = '123';
+
+// console.log(test3);
+
+// let user = 'John',
+//   age = 32,
+//   city = 'LA';
+// let some;
+
+// console.log(user, age, city, some);
+
+// --------------------------------------------------------------- Типы данных
+
+// let string = 'String'; // - строка
+// let number = 1_000_000;
+// let undefinedType = undefined;
+// let boolean = true;
+
+// let objectType = {
+//   name: 'Ihar',
+//   age: 32,
+//   city: 'Minsk',
+// };
+// let nullType = null; // Погуглить разницу/ поспрашивать чатгпт/дипсик.
+// let symbol = Symbol(1);
+// let bigNumbers = BigInt();
+
+// --------------------------------------------------------------- Циклы
+
+// let counter = 20;
+
+// while (counter >= 10) { // > < >= <= - знаки сравнения
+//   console.log(counter);
+//   counter = counter - 1;
+// }
+
+// console.log('После цикла', counter);
+
+// let counter = 0;
+
+// do {
+//   console.log(counter);
+//   counter = counter + 1;
+// } while (counter < 0);
+
+// const iterationLimit = 10;
+
+// for (
+//   let counter = 11, counter2 = 5; // создаем переменные
+//   counter < iterationLimit || counter2 === 5; // условия для работы;
+//   counter++, counter2++ // действия с созданными переменными
+// ) {
+//   console.log(counter, counter2);
+// }
+
+// && - обозначает "и"; || - обозначает "или"; === - обозначает "строгое сравнение";
+
+// console.log('11 < 1', 11 < 1);
+// console.log('5 === 5', 5 === 5);
+// console.log('11 < 1 || 5 === 5', 11 < 1 || 5 === 5);
+// console.log('11 < 1 && 5 === 5', 11 < 1 && 5 === 5);
